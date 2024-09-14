0d6669eb97954dcb1e15d733d96c4a950470ef82
lynn larson <lynn.lars20@gmail.com>
2024-09-13 20:56:24 -0400

    major css redo for home page and navbar. removed un-needed code in home and navbar as well.

--- poke/src/Home.jsx
+++ poke/src/Home.jsx
@@ -1,17 +1,21 @@
-import React, {useContext, useEffect} from "react";
-import {Context} from "./ContextProvider"
+//import React, {useContext, useEffect} from "react";
+//import {Context} from "./ContextProvider"
 import {Link} from 'react-router-dom'
 
 
-export default function Home(props){
+export default function Home(){
     //home page 
     //button to go to mypoke,get starter, and pokeList
     return(
         <div className="home">
-            <h1 className="welcome">Welcome to your pokémon app!</h1>
-            <p className="homep">Below you will be able to search for pokemon by name or type! Click their cards to see more about them!</p>
-           
-            <Link to="/pokelist"><button className="sbutton">Search Pokémon</button></Link>
+            
+            <p className="home-p">Below you will be able to search for pokemon by name or type. Click their cards to see more about them!</p>
+            <p className="disclaimer">**This is built off of an API with limited access, please note not all Pokemon are available**</p>
+            <hr/>
+            <div className='fake-card'>
+
+            </div>
+            <Link to="/pokelist"><button className="sbttn">Search Pokémon</button></Link>
             
         </div>
     )
--- poke/src/Navbar.jsx
+++ poke/src/Navbar.jsx
@@ -4,9 +4,12 @@ import {Link} from "react-router-dom"
 export default function Navbar(){
    return(
     <div className="nav">
-        <Link to="/"><button className="navbttn">Home</button></Link>
-        <Link to="/about"><button className="navbttn">About</button></Link>
-        <Link to="/starter"><button className="navbttn">Get Random</button></Link>
+        <h1 className="welcome">Pokemón Card Deck</h1>
+        <div className="bar-bttns">
+            <Link to="/"><button className="navbttn">Home</button></Link>
+            <Link to="/about"><button className="navbttn">About</button></Link>
+            <Link to="/starter"><button className="navbttn">Get Random</button></Link>
+        </div>
     </div>
    )
 }
